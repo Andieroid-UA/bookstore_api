a436c7af0f9ef5a873b2181d84dac46c9adf0e78
Andres Astra <andieroid@arizona.edu>
2024-02-08 10:41:17 -0500

    COMPLETED

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ApplicationRecord
     has_many :books, dependent: :destroy
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :author
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
 end
